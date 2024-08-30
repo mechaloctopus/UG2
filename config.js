50d621f2c1d23e8b0667d096521876c553a0cf76
mechaloctopus <mechaloctopus@gmail.com>
2024-08-29 20:33:13 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,23 @@ const SCROLL_EASING = 0.05;
 const PARALLAX_SCROLL_SPEED = 0.05;
 const NORMAL_SCROLL_SPEED = 0.5;
 
+// Image URLs
+const IMAGES = {
+    // Section 1
+    BACKGROUND: "/path/to/background.png",
+    LEFT_MOUNTAIN: "/path/to/left_mountain.png",
+    RIGHT_MOUNTAIN: "/path/to/right_mountain.png",
+    LEFT_CLOUD: "/path/to/left_cloud.png",
+    RIGHT_CLOUD: "/path/to/right_cloud.png",
+    MAN: "/path/to/man.png",
+
+    // Section 2
+    BACKGROUND2: "/path/to/background2.png",
+    TREE: "/path/to/tree.png",
+    MAN2: "/path/to/man2.png",
+
+    // Section 3
+    GEM: "/path/to/gem.png"
+};
+
 // You can add more configuration variables here as needed
