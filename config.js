4d386a7273c2efe6617c88d8824e1b0e4dc25a14
mechaloctopus <mechaloctopus@gmail.com>
2024-08-29 20:28:27 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// GIF configuration
+const GIF_URL = "https://raw.githubusercontent.com/mechaloctopus/UG2/main/4NiS.gif";
+const TOTAL_FRAMES = 30;
+
+// Scroll configuration
+const SCROLL_EASING = 0.05;
+const PARALLAX_SCROLL_SPEED = 0.05;
+const NORMAL_SCROLL_SPEED = 0.5;
+
+// You can add more configuration variables here as needed
